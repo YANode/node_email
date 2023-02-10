8404a86eebf8970fa8bfd769f597525b6bb2103f
ANode <anode@gmail.com>
2023-02-10 15:40:56 +0200

    ls-2 set up an email service

--- keys/index.js
+++ keys/index.js
@@ -2,5 +2,7 @@
 module.exports = {
     MONGODB_URI: 'mongodb+srv://anode:bOKT2JLZavt6zpY3@cluster0.o5pllfj.mongodb.net/shop',/*cloud.mongodb.com ->Database Deployments
                                                                                             ->connect-> connect your application*/
-    SESSION_SECRET: 'some secret value'
+    SESSION_SECRET: 'some secret value',
+    ENDGRID_API_KEY: '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',
+
 }
