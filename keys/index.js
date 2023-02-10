992ca5c6bbddd260afcb2ee4acbfb26bc7f85e1d
ANode <anode@gmail.com>
2023-02-10 13:31:01 +0200

    ls-1 created a configuration object

---
+++ keys/index.js
@@ -0,0 +1,6 @@
+//stores the configuration object for the application
+module.exports = {
+    MONGODB_URI: 'mongodb+srv://anode:bOKT2JLZavt6zpY3@cluster0.o5pllfj.mongodb.net/shop',/*cloud.mongodb.com ->Database Deployments
+                                                                                            ->connect-> connect your application*/
+    SESSION_SECRET: 'some secret value'
+}
